8c1520fb6c3bdf9008d46c50eef6f5ad28a7c7bc
Alexander Barth <barth.alexander@gmail.com>
2023-03-08 11:37:38 +0100

    test showing future use-case

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,16 @@
-
-using NCDatasets
-using GRIBDatasets
+import NCDatasets
+import GRIBDatasets
 using AbstractDatasets
+using Test
 
-ds2 = NCDataset("/home/abarth/ROMS-implementation-test.bak/liguriansea2019_Pair.nc")
-#dimnames(ds2["Pair"])
+datadir = joinpath(dirname(pathof(GRIBDatasets)),"..","test","sample-data")
+filename = joinpath(datadir,"era5-levels-members.grib")
+ds = GRIBDatasets.Dataset(filename)
+tmp_filename = tempname()
 
-
-
-ds = GRIBDataset("/home/abarth/.local/lib/python3.10/site-packages/xarray/tests/data/example.grib")
-
-GRIBDatasets.dimnames(ds["z"])
-
-grib_var = ds["z"]
-
-eltype(grib_var.var)
+@test_broken begin
+    NCDatasets.write(tmp_filename,ds)
+    @assert isfile(tmp_filename)
+end
 
 
-dimnames(ds["z"])
-
-
-NCDatasets.write("/tmp/test.nc",ds)
