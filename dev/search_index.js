c2094440b5689b64e92694c1b91126edc9624880
Documenter.jl <documenter@juliadocs.github.io>
2023-03-08 15:50:25 +0000

    build based on efa3605

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AbstractDatasets","category":"page"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
