6eac4a5e4926acffd7f57007bc53bbb0b44adf64
Alexander Barth <barth.alexander@gmail.com>
2023-03-29 13:04:20 +0200

    update doc

--- src/CommonDataModel.jl
+++ src/CommonDataModel.jl
@@ -12,7 +12,7 @@ NetCDF or GRIB file)
 A data set `ds` of a type derived from `AbstractDataset` should implemented at minimum:
 
 * `Base.key(ds)`: return a list of variable names a strings
-* `Base.getindex(ds,varname::String)`: return an array-like data structure (derived from `AbstractDatasetVariable`) of the variables corresponding to `varname`. This array-like data structure should follow the CF semantics.
+* `Base.getindex(ds,varname::String)`: return an array-like data structure (derived from `AbstractVariable`) of the variables corresponding to `varname`. This array-like data structure should follow the CF semantics.
 * `ds.attrib`: should be a Dict-like data structure with all global attribute names as keys and the corresponding value
 * `ds.dim`: should be a Dict-like data structure with all dimension names as keys and the corresponding length
 """
