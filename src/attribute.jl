18b1bce8e12646c7fc8756bd4b3c5e669ca9db14
Alexander Barth <barth.alexander@gmail.com>
2023-10-30 11:59:09 +0100

    expand doc strings

--- src/attribute.jl
+++ src/attribute.jl
@@ -60,7 +60,45 @@ function show_attrib(io,a)
 end
 
 
+"""
+    Base.keys(a::Attributes)
+
+Return a list of the names of all attributes.
+"""
 Base.keys(a::Attributes) = attribnames(a.ds)
+
+
+"""
+    getindex(a::Attributes,name::SymbolOrString)
+
+Return the value of the attribute called `name` from the
+attribute list `a`. Generally the attributes are loaded by
+indexing, for example:
+
+```julia
+using NCDatasets
+ds = NCDataset("file.nc")
+title = ds.attrib["title"]
+```
+"""
 Base.getindex(a::Attributes,name) = attrib(a.ds,name)
+
+
+"""
+    Base.setindex!(a::Attributes,data,name::SymbolOrString)
+
+Set the attribute called `name` to the value `data` in the
+attribute list `a`. `data` can be a vector or a scalar. A scalar
+is handeld as a vector with one element in the NetCDF data model.
+
+Generally the attributes are defined by indexing, for example:
+
+```julia
+ds = NCDataset("file.nc","c")
+ds.attrib["title"] = "my title"
+close(ds)
+```
+"""
 Base.setindex!(a::Attributes,data,name) = defAttrib(a.ds,name,data)
+
 Base.show(io::IO,a::Attributes) = show_attrib(io,a)
--- src/dimension.jl
+++ src/dimension.jl
@@ -2,7 +2,15 @@
 """
     CommonDatamodel.dimnames(ds::AbstractDataset)
 
-Return an iterable of all dimension names in `ds`.
+Return an iterable of all dimension names in `ds`. This
+information can also be accessed using the property `ds.dim`:
+
+# Examples
+
+```julia
+ds = NCDataset("results.nc", "r");
+dimnames = keys(ds.dim)
+```
 """
 dimnames(ds::Union{AbstractDataset,AbstractVariable}) = ()
 
