f9df8ebaf1485a9ed7f6ba8c59dc15fd4314c9f8
lupemba <simon.kok@live.dk>
2025-06-27 14:18:50 +0200

    Update documentation and add MetopDatasets.jl to integration tests

--- src/types.jl
+++ src/types.jl
@@ -10,7 +10,7 @@ NetCDF or GRIB file)
 
 A data set `ds` of a type derived from `AbstractDataset` should implemented at minimum:
 
-* `Base.key(ds)`: return a list of variable names as strings
+* `varnames(ds)`: return a list of variable names as strings
 * `variable(ds,varname::String)`: return an array-like data structure (derived from `AbstractVariable`) of the variables corresponding to `varname`. This array-like data structure should follow the CF semantics.
 * `dimnames(ds)`: should be an iterable with all dimension names in the data set  `ds`
 * `dim(ds,name)`: dimension value corresponding to name
@@ -33,13 +33,14 @@ end
 
 
 """
-`AbstractVariable{T,N}` is a subclass of `AbstractArray{T, N}`. A variable `v` of a type derived from `AbstractVariable` should implement:
+`AbstractVariable{T,N}` is a subclass of `DiskArrays.AbstractDiskArray{T,N}` which is a subclass of `AbstractArray{T, N}`.
+A variable `v` of a type derived from `AbstractVariable` should implement:
 
 * `name(v)`: should be the name of variable within the data set
 * `dimnames(v)`: should be a iterable data structure with all dimension names of the variable `v`
 * `dataset(v)`: the parent dataset containing `v`
 * `Base.size(v)`: the size of the variable
-* `Base.getindex(v,indices...)`: get the data of `v` at the provided indices
+* `DiskArrays.readblock!(v, aout, indices...)`: get the data of `v` at the provided indices. See [DiskArrays.jl readme](https://github.com/JuliaIO/DiskArrays.jl) for more details.
 
 Optionally a variable can have attributes:
 
@@ -48,7 +49,7 @@ Optionally a variable can have attributes:
 
 For a writable dataset, one should also implement:
 * `defAttrib`: define a attribute
-* `Base.setindex!(v,data,indices...)`: set the data in `v` at the provided indices
+* `DiskArrays.writeblock!(v,ain,indices...)`: set the `ain` in `v` at the provided indices. See [DiskArrays.jl readme](https://github.com/JuliaIO/DiskArrays.jl) for more details.
 
 """
 abstract type AbstractVariable{T,N} <: AbstractDiskArray{T, N}
